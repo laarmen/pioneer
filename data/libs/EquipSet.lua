ba52d7d6f601a04062689ce7320a39be6cb4cc0f
Simon Chopin <chopin.simon@gmail.com>
2012-11-14 16:27:56 +0100

    EquipSet.lua: Replace all the 'equipment' parameters by 'item'

--- data/libs/EquipSet.lua
+++ data/libs/EquipSet.lua
@@ -80,7 +80,7 @@ end
 --
 -- Parameters:
 --
---  equipment - The equipment to count.
+--  item - The equipment to count.
 --
 --  slots - List of the slots to check. You can also provide a string if it
 --          is only one slot. If this argument is not provided, all slots
@@ -90,7 +90,7 @@ end
 -- 
 --  free_space - The available space (integer)
 --
-function EquipSet:Count(equipment, slots)
+function EquipSet:Count(item, slots)
 	if type(slots) == "table" then
 		local to_check = {}
 		for _, s in ipairs(slots) do
@@ -105,7 +105,7 @@ function EquipSet:Count(equipment, slots)
 	local count = 0
 	for _, slot in ipairs(to_check) do
 		for _, e in ipairs(slot) do
-			if e == equipment then
+			if e == item then
 				count = count + 1
 			end
 		end
@@ -127,7 +127,7 @@ end
 --
 --  Please refer to the Remove method.
 --
-function EquipSet:__Remove_NoCheck (equipment, num, slot)
+function EquipSet:__Remove_NoCheck (item, num, slot)
 	local s = self.slots[slot]
 	if not s or s.__occupied == 0 then
 		return 0
@@ -137,7 +137,7 @@ function EquipSet:__Remove_NoCheck (equipment, num, slot)
 		if removed >= num or s.__occupied <= 0 then
 			return removed
 		end
-		if s[i] == equipment then
+		if s[i] == item then
 			s[i] = nil
 			removed = removed + 1
 			s.__occupied = s.__occupied - 1
@@ -160,7 +160,7 @@ end
 --
 --  Please refer to the Add method.
 --
-function EquipSet:__Add_NoCheck(equipment, num, slot)
+function EquipSet:__Add_NoCheck(item, num, slot)
 	if self:FreeSpace(slot) == 0 then
 		return 0
 	end
@@ -171,7 +171,7 @@ function EquipSet:__Add_NoCheck(equipment, num, slot)
 			return added
 		end
 		if not s[i] then
-			s[i] = equipment
+			s[i] = item
 			added = added + 1
 			s.__occupied = s.__occupied + 1
 		end
@@ -186,7 +186,7 @@ end
 --
 -- Parameters:
 --
---  equipment - the equipment to install
+--  item - the equipment to install
 --  num - the number of pieces to install. If nil, only one will be installed.
 --  slot - the slot where to install the equipment. It will be checked against
 --         the equipment itself, the method will return -1 if the slot isn't
@@ -197,24 +197,24 @@ end
 --  installed - the number of pieces actually installed, or -1 if the specified
 --              slot is not valid.
 --
-function EquipSet:Add(ship, equipment, num, slot)
-	local num = num or 1
+function EquipSet:Add(ship, item, num, slot)
+	num = num or 1
 	if not slot then
-		local slot = equipment:GetDefaultSlot(ship)
-	elseif not equipment:IsValidSlot(ship, slot) then
+		slot = item:GetDefaultSlot(ship)
+	elseif not item:IsValidSlot(ship, slot) then
 		return -1
 	end
 
-	local added = self:__Add_NoCheck(equipment, num, slot)
+	local added = self:__Add_NoCheck(item, num, slot)
 	if added == 0 then
 		return 0
 	end
-	local postinst_diff = added - equipment:Install(ship, num, slot)
+	local postinst_diff = added - item:Install(ship, num, slot)
 	if postinst_diff <= 0 then
 		return added
 	end
 
-	self:__Remove_NoCheck(equipment, postinst_diff, slot)
+	self:__Remove_NoCheck(item, postinst_diff, slot)
 	return added - postinst_diff
 end
 
@@ -224,7 +224,7 @@ end
 --
 -- Parameters:
 --
---  equipment - the equipment to remove.
+--  item - the equipment to remove.
 --  num - the number of pieces to uninstall. If nil, only one will be removed.
 --  slot - the slot where to install the equipment. If nil, the default slot
 --         for the equipment will be used.
@@ -233,21 +233,21 @@ end
 --
 --  removed - the number of pieces actually removed.
 --
-function EquipSet:Remove(ship, equipment, num, slot)
+function EquipSet:Remove(ship, item, num, slot)
 	local num = num or 1
 	if not slot then
-		local slot = equipment:GetDefaultSlot(ship)
+		local slot = item:GetDefaultSlot(ship)
 	end
-	local removed = self:__Remove_NoCheck(equipment, num, slot)
+	local removed = self:__Remove_NoCheck(item, num, slot)
 	if removed == 0 then
 		return 0
 	end
-	local postuninstall_diff = removed - equipment:Uninstall(ship, num, slot)
+	local postuninstall_diff = removed - item:Uninstall(ship, num, slot)
 	if postuninstall_diff <= 0 then
 		return removed
 	end
 
-	self:__Add_NoCheck(equipment, postuninstall_diff, slot)
+	self:__Add_NoCheck(item, postuninstall_diff, slot)
 	return removed - postuninstall_diff
 end
 
@@ -262,14 +262,14 @@ function EquipSet:Get(slot, index)
 	return ret
 end
 
-function EquipSet:Set(ship, slot, index, equipment)
+function EquipSet:Set(ship, slot, index, item)
 	if index < 1 or index > # (self.slots[slot]) then
 		error("EquipSet:Set(): argument 'index' out of range")
 	end
 	to_remove = self.slots[slot][index]
 	if not to_remove or to_remove:Uninstall(ship, 1, slot) == 1 then
-		if equipment:Install(ship, 1, slot) == 1 then
-			self.slots[slot][index] = equipment
+		if item:Install(ship, 1, slot) == 1 then
+			self.slots[slot][index] = item
 		else -- Rollback the uninstall
 			to_remove:Install(ship, 1, slot)
 		end
